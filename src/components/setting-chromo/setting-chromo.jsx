e6918022d420111abfb7588e6d3c5c9e449751c6
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2026-02-11 00:09:42 +0000

    setting style

--- src/components/setting-chromo/setting-chromo.jsx
+++ src/components/setting-chromo/setting-chromo.jsx
@@ -6,39 +6,54 @@ const SettingChromo = ({asa, asa2, asa3, hronomer, marker, screen}) => {
     const [markerSetting, setMarkerSetting] = useState(marker);
     const [screenSetting, setScreenSetting] = useState(screen);
 
+    function active(value) {     
+        const activiti = Number(value);
+        return activiti ? 'setting__wrapper' : 'setting__wrapper unActive';
+    }
+
     return (
+        <>
+        <h1 className="setting__h1">Here you can try to use your setting</h1>
         <div className="setting">
+
             <div className="setting__block">
-                <label htmlFor="time-code">Time code</label>
-                <span>Not</span>
-                <input type="range" id="time-code" min={0} max={1} value={timecode} onChange={(e) => {
-                    setTimecode(e.target.value);
-                    asa(e.target.value);
-                    }}/>
-                <span>Yes</span>
+                <label className="setting__title" htmlFor="time-code">Time code</label>
+                <div className={active(timecode)}>
+                    <span className="setting__span">Not</span>
+                    <input className="setting__range" type="range" id="time-code" min={0} max={1} value={timecode} onChange={(e) => {
+                        setTimecode(e.target.value);
+                        asa(e.target.value);
+                        }}/>
+                    <span className="setting__span">Yes</span>
+                </div>
             </div>
 
              <div className="setting__block">
-                <label htmlFor="marker-code">Marker</label>
-                <span>Not</span>
-                <input type="range" id="marker-code" min={0} max={1} value={markerSetting} onChange={(e) => {
-                    setMarkerSetting(e.target.value);
-                    asa2(e.target.value);
-                    }}/>
-                <span>Yes</span>
+                    <label className="setting__title" htmlFor="marker-code">Marker</label>
+                    <div className={active(markerSetting)}>
+                    <span className="setting__span">Not</span>
+                    <input className="setting__range" type="range" id="marker-code" min={0} max={1} value={markerSetting} onChange={(e) => {
+                        setMarkerSetting(e.target.value);
+                        asa2(e.target.value);
+                        }}/>
+                    <span className="setting__span">Yes</span>
+                </div>
             </div>
 
              <div className="setting__block">
-                <label htmlFor="screen-code">Screen</label>
-                <span>blue</span>
-                <input type="range" id="screen-code" min={0} max={1} value={screenSetting} onChange={(e) => {
-                    setScreenSetting(e.target.value);
-                    asa3(e.target.value);
-                    }}/>
-                <span>green</span>
+                    <label className="setting__title" htmlFor="screen-code">Screen</label>
+                    <div className={active(screenSetting)}>
+                    <span className="setting__span">blue</span>
+                    <input className="setting__range" type="range" id="screen-code" min={0} max={1} value={screenSetting} onChange={(e) => {
+                        setScreenSetting(e.target.value);
+                        asa3(e.target.value);
+                        }}/>
+                    <span className="setting__span">green</span>
+                </div>
             </div>
          
         </div>
+        </>
     )
 }
 
